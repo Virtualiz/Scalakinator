606d03f55766e649f63d801a90ed58fb293b0873
sinteff3u <bruno.sinteff@etu.univ-lorraine.fr>
2016-03-16 16:52:02 +0100

    main qui fait fichierToABanimal

--- src/Main.scala
+++ src/Main.scala
@@ -4,14 +4,23 @@ object Main {
   
   trait ABanimal
   case class Animal(nom:String) extends ABanimal
-  case class Question(q:String,oui:ABanimal,non:ABanimal)
+  case class Question(q:String,oui:ABanimal,non:ABanimal) extends ABanimal
   
   def fichierToABanimal(nomf:String):ABanimal = {
     val l = Source.fromFile(nomf).getLines.toList
-    def convert(l:List[String]): ABanimal = l match {
-      case _ => new Animal("bonjour")
+    def convert(l:List[String]): (ABanimal,List[String]) = l match {
+      case t::l if t contains "q:" => { 
+        val (oui,l2)= convert(l)
+        val (non,l3)= convert(l2)
+        (new Question(t,oui,non),l3)
+      }
+      case t::q => (new Animal(t),q)
     }
-    convert(l)
+    convert(l)._1
   }
+ 
   
+  def main(args:Array[String]){
+    println(fichierToABanimal("Source"))
+  }
 }
